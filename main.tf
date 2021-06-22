942dd9136e5674780f20d9308ac4faf1188a7888
nitrocode <nitrocode@users.noreply.github.com>
2021-06-22 15:38:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ data "aws_iam_policy_document" "service_account_assume_role" {
 }
 
 resource "aws_iam_policy" "service_account" {
-  for_each    = toset(compact([module.service_account_label.id]))
+  for_each    = var.aws_iam_policy_document != null ? toset(compact([module.service_account_label.id])) : []
   name        = each.value
   description = format("Grant permissions to EKS ServiceAccount %s", local.service_account_id)
   policy      = coalesce(var.aws_iam_policy_document, "{}")
