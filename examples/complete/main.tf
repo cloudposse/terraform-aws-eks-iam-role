e17e353541c37588b5cd7ec4cfaef97de8a35cab
nitrocode <nitrocode@users.noreply.github.com>
2021-12-21 10:53:48 -0600

    Keep example and README using list(string)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -14,7 +14,7 @@ module "autoscaler_role" {
   # Rather than create a whole cluster, just fake the OIDC URL
   # eks_cluster_oidc_issuer_url = module.eks_cluster.eks_cluster_identity_oidc_issuer
   eks_cluster_oidc_issuer_url = "https://oidc.eks.us-west-2.amazonaws.com/id/FEDCBA9876543210FEDCBA9876543210"
-  aws_iam_policy_document     = data.aws_iam_policy_document.autoscaler.json
+  aws_iam_policy_document     = [data.aws_iam_policy_document.autoscaler.json]
 
   context = module.this.context
 }
@@ -54,7 +54,7 @@ module "cert-manager_role" {
   # Rather than create a whole cluster, just fake the OIDC URL
   # eks_cluster_oidc_issuer_url = module.eks_cluster.eks_cluster_identity_oidc_issuer
   eks_cluster_oidc_issuer_url = "https://oidc.eks.us-west-2.amazonaws.com/id/FEDCBA9876543210FEDCBA9876543210"
-  aws_iam_policy_document     = data.aws_iam_policy_document.cert-manager.json
+  aws_iam_policy_document     = [data.aws_iam_policy_document.cert-manager.json]
 
   context = module.this.context
 }
