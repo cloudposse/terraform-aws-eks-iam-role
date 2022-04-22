3e16d58af3cba8c5edb9b062bfe6553bb6906815
Pepe Barbe <pepe@antropoide.net>
2022-04-22 14:49:42 -0500

    Refactor enable logic to use counts instead of `for_each` (#27)

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ locals {
 
   eks_cluster_oidc_issuer = replace(var.eks_cluster_oidc_issuer_url, "https://", "")
 
-  aws_account_number = coalesce(var.aws_account_number, data.aws_caller_identity.current.account_id)
+  aws_account_number = local.enabled ? coalesce(var.aws_account_number, data.aws_caller_identity.current[0].account_id) : ""
 
   # If both var.service_account_namespace and var.service_account_name are provided,
   # then the role ARM will have one of the following formats:
@@ -29,9 +29,12 @@ locals {
 
   # Try to return the first element, if that doesn't work, try the tostring approach
   aws_iam_policy_document = try(var.aws_iam_policy_document[0], tostring(var.aws_iam_policy_document), "{}")
+  iam_policy_enabled      = local.enabled && length(var.aws_iam_policy_document) > 0
 }
 
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+  count = local.enabled ? 1 : 0
+}
 
 module "service_account_label" {
   source  = "cloudposse/label/null"
@@ -48,15 +51,15 @@ module "service_account_label" {
 }
 
 resource "aws_iam_role" "service_account" {
-  for_each           = toset(compact([module.service_account_label.id]))
-  name               = each.value
+  count              = local.enabled ? 1 : 0
+  name               = module.service_account_label.id
   description        = format("Role assumed by EKS ServiceAccount %s", local.service_account_id)
-  assume_role_policy = data.aws_iam_policy_document.service_account_assume_role[each.value].json
+  assume_role_policy = data.aws_iam_policy_document.service_account_assume_role[0].json
   tags               = module.service_account_label.tags
 }
 
 data "aws_iam_policy_document" "service_account_assume_role" {
-  for_each = toset(compact([module.service_account_label.id]))
+  count = local.enabled ? 1 : 0
 
   statement {
     actions = [
@@ -79,15 +82,15 @@ data "aws_iam_policy_document" "service_account_assume_role" {
 }
 
 resource "aws_iam_policy" "service_account" {
-  for_each    = length(var.aws_iam_policy_document) > 0 ? toset(compact([module.service_account_label.id])) : []
-  name        = each.value
+  count       = local.iam_policy_enabled ? 1 : 0
+  name        = module.service_account_label.id
   description = format("Grant permissions to EKS ServiceAccount %s", local.service_account_id)
   policy      = local.aws_iam_policy_document
   tags        = module.service_account_label.tags
 }
 
 resource "aws_iam_role_policy_attachment" "service_account" {
-  for_each   = length(var.aws_iam_policy_document) > 0 ? toset(compact([module.service_account_label.id])) : []
-  role       = aws_iam_role.service_account[each.value].name
-  policy_arn = aws_iam_policy.service_account[each.value].arn
+  count      = local.iam_policy_enabled ? 1 : 0
+  role       = aws_iam_role.service_account[0].name
+  policy_arn = aws_iam_policy.service_account[0].arn
 }
